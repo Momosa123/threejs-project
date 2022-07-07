b5b74ac1ad03e82d927b70d8629468244e5f822f
Momosa123 <mouhamadoul.sarr@gmail.com>
2022-07-07 11:56:57 +0200

    adding my picture

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import './style.css'
 import * as THREE from 'three'
-import { GridHelper } from 'three'
+import { GridHelper, SphereBufferGeometry } from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 const scene = new THREE.Scene()
-
-const zSaturne = 500
-const ysaturne = 200
+import vShader from './vertexShader.glsl'
+import fShader from './fragmentShader.glsl'
+const xSaturne= 500
+const zSaturne = 0
+const ysaturne = 250
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
 
 const renderer = new THREE.WebGL1Renderer({
@@ -23,7 +25,7 @@ const geometryOrange = new THREE.TorusGeometry(16, 0.75, 2, 100)
 const materialOrange = new THREE.MeshStandardMaterial({color:0xFF6347})
 const torusOrange = new THREE.Mesh(geometryOrange, materialOrange)
 torusOrange.position.z = zSaturne
-torusOrange.position.x = -30
+torusOrange.position.x = xSaturne
 torusOrange.position.y = ysaturne
 scene.add(torusOrange)
 
@@ -31,7 +33,7 @@ const geometryBlue = new THREE.TorusGeometry(14, 0.75, 2, 100)
 const materialBlue = new THREE.MeshStandardMaterial({color:0x189AB4})
 const torusBlue  = new THREE.Mesh(geometryBlue, materialBlue)
 torusBlue.position.z = zSaturne
-torusBlue.position.x = -30
+torusBlue.position.x = xSaturne
 torusBlue.position.y = ysaturne
 scene.add(torusBlue)
 
@@ -39,7 +41,7 @@ const geometryGray = new THREE.TorusGeometry(12, 0.75, 2, 100)
 const materialGray = new THREE.MeshStandardMaterial({color:0xced4da})
 const torusGray  = new THREE.Mesh(geometryGray, materialGray)
 torusGray.position.z = zSaturne
-torusGray.position.x = -30
+torusGray.position.x = xSaturne
 torusGray.position.y = ysaturne
 scene.add(torusGray)
 
@@ -70,10 +72,31 @@ const spaceTexture = new THREE.TextureLoader().load('space.jpg')
 const normalTexture = new THREE.TextureLoader().load('normal.jpg')
 scene.background = spaceTexture
 
+//Custom fragment
+const customShaderMaterial = new THREE.ShaderMaterial({
+  vertexShader: vShader,
+  fragmentShader: fShader
+})
 
 
+const cubePicture = new THREE.TextureLoader().load('IMG_1904.jpg')
+const cubeMesh = new THREE.Mesh(
+  new THREE.BoxGeometry(60,60,60),
+  new THREE.MeshStandardMaterial({
+    map: cubePicture
+    }
+  )
+)
 const sunTexture = new THREE.TextureLoader().load('sun.jpeg')
  
+
+cubeMesh.position.z = 150
+cubeMesh.position.x = -250
+cubeMesh.position.y = 150
+
+scene.add(cubeMesh)
+
+//adding the sun
 const sun = new THREE.Mesh(
   new THREE.SphereGeometry(30, 32, 32),
   new THREE.MeshStandardMaterial({
@@ -83,14 +106,14 @@ const sun = new THREE.Mesh(
   )
 )
 
+
 scene.add(sun)
 const earthTexture = new THREE.TextureLoader().load('earth.jpg')
  
 const earth = new THREE.Mesh(
   new THREE.SphereGeometry(10, 32, 32),
   new THREE.MeshStandardMaterial({
-    map: earthTexture,
-    normalMap: normalTexture
+    map: earthTexture
     }
   )
 )
@@ -132,7 +155,7 @@ const saturne = new THREE.Mesh(
 )
 
 saturne.position.z = zSaturne
-saturne.position.x = -30
+saturne.position.x = xSaturne
 saturne.position.y = ysaturne
 scene.add(saturne)
 
@@ -163,6 +186,10 @@ function animate(){
   torusGray.rotation.x+=0.01;
   torusGray.rotation.y+=0.005;
   torusGray.rotation.z+=0.01;
+
+  cubeMesh.rotation.x+=0.01;
+  cubeMesh.rotation.y+=0.005;
+  cubeMesh.rotation.z+=0.01;
  
   earth.rotation.y+=0.01;
   
